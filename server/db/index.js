ca1c9204aae139a0fc41b0e2c8bd625508d6621a
hanyen <me@hanyen.com>
2017-01-13 20:36:55 -0800

    Database and tables created. Working on Express.

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,19 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var con = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
+
+con.connect(function(err) {
+  if (err) {
+    console.log('Error connecting to database');
+    return;
+  }
+  console.log('Connecntion established');
+});
+
+con.end(function(err) {});
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,77 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
+    
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` VARCHAR NULL DEFAULT NULL,
+  `createdat` DATETIME NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  `id_rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+
 
 /* Create other tables and define schemas for them here! */
 
 
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` CHAR(15) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` CHAR(15) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`message`,`createdat`,`id_users`,`id_rooms`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`roomname`) VALUES
+-- ('','');
 
 
 /*  Execute this file from the command line by typing:
